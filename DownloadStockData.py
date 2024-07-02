69ffb3a2cd462a37c550f264d4581207f4ff400d
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-02 17:20:00 -0300

    Ajustanto formatação das datas

--- DownloadStockData.py
+++ DownloadStockData.py
@@ -5,12 +5,15 @@ import time
 from_date = input('Enter start date in dd/mm/yyyy format : ')
 to_date = input('Enter end date in dd/mm/yyyy format : ')
 
-url = "https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1=34542700&period2=1642032000&interval=1d&events=history&includeAdjustedClose=true"
+
+from_datime = datetime.strptime(from_date, 'Y%/%m/%d')
+to_datetime = datetime.strptime(to_date, 'Y%/%m/%d')
+
+from_epoch = time.mktime(from_datime.timetuple())
+to_epoch = time.mktime(to_datetime.timetuple())
+
+url = f"https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
-
-
-from_datime = datetime.strptime(from_date, 'm%/%d/%Y')
-to_datetime = datetime.strptime(to_date, 'm%/%d/%Y')
 
 content = requests.get(url, headers=headers).content
 print(content)
