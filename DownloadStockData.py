8a4af0dbaface991b74524df4238e11085eb7fc8
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-02 17:23:06 -0300

    add ticker simbol input

--- DownloadStockData.py
+++ DownloadStockData.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime
 import time
 
+ticker = input("Enter the ticker symbol: ")
 from_date = input('Enter start date in dd/mm/yyyy format : ')
 to_date = input('Enter end date in dd/mm/yyyy format : ')
 
@@ -12,7 +13,7 @@ to_datetime = datetime.strptime(to_date, 'Y%/%m/%d')
 from_epoch = time.mktime(from_datime.timetuple())
 to_epoch = time.mktime(to_datetime.timetuple())
 
-url = f"https://query1.finance.yahoo.com/v7/finance/download/AAPL?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
+url = f"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={from_epoch}&period2={to_epoch}&interval=1d&events=history&includeAdjustedClose=true"
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
 
 content = requests.get(url, headers=headers).content
