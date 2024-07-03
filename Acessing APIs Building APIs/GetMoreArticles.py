628cf039449a56f426d37ca3bc8d57f25ec91b5d
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-03 16:32:09 -0300

    Aplicação de boas praticas na sequencia logica de GetNews_to_ExcelFile

--- Acessing APIs Building APIs/GetMoreArticles.py
+++ Acessing APIs Building APIs/GetMoreArticles.py
@@ -1,14 +1,17 @@
 import requests
 
 api_Key = "80423fcb354f45ea8fb17e14b61fc04a"
+api_link = "https://newsapi.org/v2/everything?"
+sort_by = "popularity"
+
 topic = input("Tópico: ")
 from_date = input("A partir da data: ")
 
 def get_news(topic, from_date , api_Key):
-    url = (f'https://newsapi.org/v2/everything?'
+    url = (f'{api_link}'
            f'q={topic}&'
            f'from={from_date}&'
-           f'sortBy=popularity&'
+           f'sortBy={sort_by}&'
            f'apiKey={api_Key}')
     response = requests.get(url)
     print(response.json())
---
+++ Improve knowledge/GetNews_to_ExelFile.py
@@ -0,0 +1,24 @@
+import requests
+
+api_link = "https://newsapi.org/v2/everything?"
+
+topic = input("Tópico: ")
+from_date = input("A partir da data: ")
+
+sort_by = "popularity"
+api_Key = "80423fcb354f45ea8fb17e14b61fc04a"
+
+
+def get_news(api_link, topic, from_date , sort_by, api_Key):
+    url = (f'{api_link}'
+           f'q={topic}&'
+           f'from={from_date}&'
+           f'sortBy={sort_by}&'
+           f'apiKey={api_Key}')
+    response = requests.get(url)
+    print(response.json())
+
+def main():
+    get_news(api_link, topic, from_date , sort_by, api_Key)
+
+main()
