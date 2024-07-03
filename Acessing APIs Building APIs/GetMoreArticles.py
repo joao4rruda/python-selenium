962670afb3984c2e5e0dd6b3e68d4f175d03fb78
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-03 16:16:30 -0300

    Consumindo API com python

---
+++ Acessing APIs Building APIs/GetMoreArticles.py
@@ -0,0 +1,16 @@
+import requests
+
+api_Key = "80423fcb354f45ea8fb17e14b61fc04a"
+topic = input("Tópico: ")
+from_date = input("A partir da data: ")
+
+def get_news(topic, from_date, api_Key):
+    url = (f'https://newsapi.org/v2/everything?'
+           f'q={topic}&'
+           f'from={from_date}&'
+           f'sortBy=popularity&'
+           f'apiKey={api_Key}')
+    response = requests.get(url)
+    print(response.json())
+
+get_news(topic, from_date, api_Key)
