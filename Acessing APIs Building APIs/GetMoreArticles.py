4a2fba65cc95195c8802b1c2a67a94fea13c4314
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-03 16:25:15 -0300

    Preparando arquivo GetNews_to_ExelFile

--- Acessing APIs Building APIs/GetMoreArticles.py
+++ Acessing APIs Building APIs/GetMoreArticles.py
@@ -4,7 +4,7 @@ api_Key = "80423fcb354f45ea8fb17e14b61fc04a"
 topic = input("Tópico: ")
 from_date = input("A partir da data: ")
 
-def get_news(topic, from_date, api_Key):
+def get_news(topic, from_date , api_Key):
     url = (f'https://newsapi.org/v2/everything?'
            f'q={topic}&'
            f'from={from_date}&'
