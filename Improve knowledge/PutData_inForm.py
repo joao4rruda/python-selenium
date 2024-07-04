83f9dd6f2a5e26d501b7d0b2cec05a2e6129a4a4
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-04 14:53:10 -0300

    Inserindo novos articles e organizando data

--- Improve knowledge/PutData_inForm.py
+++ Improve knowledge/PutData_inForm.py
@@ -8,58 +8,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
 
-# region request API
-
-api_link = "https://newsapi.org/v2/everything?"
-api_key = "80423fcb354f45ea8fb17e14b61fc04a"
-
-def get_news(api_link, topic, from_date, sort_by, api_key):
-    url = f"{api_link}q={topic}&from={from_date}&sortBy={sort_by}&apiKey={api_key}"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        content = response.json()
-        articles = content.get('articles', [])
-        return articles
-    else:
-        print(f"Failed to fetch new. Status code: {response.status_code}")
-        return []
-    
-def write_news_to_csv(articles):
-    if not articles:
-        print("No articles to write.")
-        return
-    
-    data = [(article['author'], article['title'], article['description'], article['content']) for article in articles]
-    
-    for author, title, description, content in data:
-        put_in_form(author , title, description, content)
-
-    # data_frame = pd.DataFrame(data, columns=['Title', 'Description'])
-    # data_frame.to_csv('new.csv', index=False, encoding='utf-8')
-    # print("News written to news.csv")
-
-
-# region processo de automação
-
-def put_in_form(author, title, description, content):
-    if author != "[Removed]" and title != "[Removed]" and content != "[Removed]":
-        driver = get_driver()
-        # Author
-        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(author) 
-        # Title
-        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(title)
-        time.sleep(4)
-        # Description
-        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys(description)
-        time.sleep(4)
-        # Content
-        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys(content)
-        time.sleep(4)
-
-        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span').click()
-        
-
+# region GET DRIVER
 
 def get_driver():
     options = webdriver.ChromeOptions()
@@ -82,6 +31,7 @@ def get_driver():
     driver.get("https://docs.google.com/forms/d/e/1FAIpQLScswuDL2q7FS1OI2Cru3NY1X9kpBZiFN9F7f8t45JlXB712yA/viewform")
     return driver
 
+# region MAIN
 def main():
 
     topic = input("Tópico: ")
@@ -89,12 +39,63 @@ def main():
     sort_by = "popularity"
 
     articles = get_news(api_link, topic, from_date, sort_by, api_key)
-    write_news_to_csv(articles)
+    organize_data_to_write(articles)
 
-    driver = get_driver()
 
-    if __name__ == "__main__":
-        main()
+# region GET NEWS
 
+api_link = "https://newsapi.org/v2/everything?"
+api_key = "80423fcb354f45ea8fb17e14b61fc04a"
+
+def get_news(api_link, topic, from_date, sort_by, api_key):
+    url = f"{api_link}q={topic}&from={from_date}&sortBy={sort_by}&apiKey={api_key}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.json()
+        articles = content.get('articles', [])
+        return articles
+    else:
+        print(f"Failed to fetch new. Status code: {response.status_code}")
+        return []
+    
+def organize_data_to_write(articles):
+    if not articles:
+        print("No articles to write.")
+        return
+    
+    data = [(article['author'], 
+             article['title'], 
+             article['description'], 
+             article['content'],
+             article['url'],
+             article['urlToImage'],
+             article['publishedAt']) for article in articles]
+    
+    for author, title, description, content in data:
+        put_in_form(author , title, description, content)
+
+# region AUTOMATION FORM
+
+def put_in_form(author, title, description, content, url, urlToImage, publishedAt):
+    if author != "[Removed]" and title != "[Removed]" and content != "[Removed]":
+        driver = get_driver()
+        # Author
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(author) 
+        # Title
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(title)
+        # Description
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys(description)
+        # Content
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[2]/textarea').send_keys(content)
+        # URL 
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(url)
+        # url da imagem
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(urlToImage)
+
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[7]/div/div/div[2]/div/div[1]/div/div[1]/input').send_keys(publishedAt)
+        # publicado em
+
+        driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span').click()
 
 main()
