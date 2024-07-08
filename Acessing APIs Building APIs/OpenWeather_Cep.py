39e92da8d0be526ab00f2d3ceb38d470a60ee8ee
joao4rruda <joao.arruda.corp@gmail.com>
2024-07-08 15:46:57 -0300

    Novo arquivo

---
+++ Acessing APIs Building APIs/OpenWeather_Cep.py
@@ -0,0 +1,7 @@
+import requests
+from pprint import pprint
+
+API_key = "de84a73c7b7e4f5c4ceba27920c9ff19"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+zip_code = input("Enter a ZI")
